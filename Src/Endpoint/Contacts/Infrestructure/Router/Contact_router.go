990fd480cab4627cbbfc709df1818061bb3264f5
Somer-H <hsomdev19@gmail.com>
2026-02-26 14:46:48 -0600

    search

--- Src/Endpoint/Contacts/Infrestructure/Router/Contact_router.go
+++ Src/Endpoint/Contacts/Infrestructure/Router/Contact_router.go
@@ -10,11 +10,13 @@ func RouterContact(g *gin.Engine,
 	getAllContactsController *controller.GetAllContactsController,
 	createContactController *controller.CreateContactController,
 	deleteContactController *controller.DeleteContactController,
-){
+	getContactByNameController *controller.GetContactByNameController,
+) {
 	contactGroup := g.Group("/api/v1/contacts")
 	{
 		contactGroup.GET("/getAll/:userId", getAllContactsController.Handle)
 		contactGroup.POST("/create", createContactController.Handle)
 		contactGroup.DELETE("/delete/:userId/:contactId", deleteContactController.Handle)
+		contactGroup.GET("/search/:username", getContactByNameController.Handle)
 	}
 }
